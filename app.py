c682c306bd10562764df285209549680dcf86f7e
tobontob <tobontob@naver.com>
2025-07-07 00:10:07 +0900

    up

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from typing import List
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+client = MongoClient(MONGODB_URI)
+db = client["news_db"]
+result_col = db["analyzed_news"]
+
+app = FastAPI()
+
+# CORS 허용 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 배포시에는 프론트엔드 도메인만 허용 권장
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/analyzed_news")
+def get_analyzed_news():
+    news_list = list(result_col.find().sort("published", -1).limit(20))
+    for news in news_list:
+        news["_id"] = str(news["_id"])
+    return {"news": news_list} 
--- news_crawler/utils.py
+++ news_crawler/utils.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+ 
 def save_news_to_mongo(news_list, mongo_uri, db_name="news_db", collection="raw_news"):
     client = MongoClient(mongo_uri)
     db = client[db_name]
